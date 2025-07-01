12e99e3892aad16faf29841b141bb24ae3dc7c04
User <asqarservis0000@gmail.com>
2025-07-01 13:03:33 +0300

    yangilandi

--- DjangoTodo/urls.py
+++ DjangoTodo/urls.py
@@ -22,5 +22,4 @@ from DjangoTodo.settings import MEDIA_URL, MEDIA_ROOT
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.urls')),
-    path('', include('todo.urls')),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
